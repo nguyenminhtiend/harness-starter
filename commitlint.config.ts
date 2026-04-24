081cf2abc8d39de7e6311366dd490cb7b97b2eba
Tien Nguyen <tien.nguyen@wearecare.sg>
2026-04-24 19:54:20 +0700

    chore: increase commitlint header-max-length to 200 characters

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from '@commitlint/types';
 
 const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 200],
+  },
 };
 
 export default config;
